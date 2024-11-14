936dab49f9f5730032e7951761c4c869026f3190
Martseniuk Alina <martseniuk.av@gmail.com>
2024-11-14 19:44:34 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+//*====================== завдання 1 =================*\\
+const select = document.querySelector('[name="select"]')
+console.dir(select);
+
+select.addEventListener('click', () => (
+    e.preventDefult
+))
+
+//*====================== завдання 2 =================*\\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
